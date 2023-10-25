0efa27ce0686da2e2c3600958000f80700f04c72
Clarissa_Benitez <ycv3fh@Virginia.edu>
2023-10-25 17:39:29 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="Demo",
+    version="0.1",
+    description="a demo package",
+    url="https://github.com/ycv3fh/MyPackageEx",
+    author="Clarissa Benitez",
+    author_email="ycv3fh@Virginia.edu",
+    licence="N/A",
+    packages=['MyPackageEx']
+)
